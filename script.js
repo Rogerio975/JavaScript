d7b26f60e26a241946ea472d6b3211f7e466eb34
Rogerio975 <57407676+Rogerio975@users.noreply.github.com>
2024-04-25 17:20:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let meuDiv = document.getElementById("meuDiv");
+let minhaClasse = document.getElementsByClassName("minhaClasse")[0];
+let h1 = document.getElementsByTagName("h1")[0];
+
+let meuDiv2 = document.querySelector("#meuDiv2");
+let minhaClasse2 = document.querySelector(".minhaClasse");
+let h1_2 = document.querySelector("h1");
+
+meuDiv.textContent = "Novo conteúdo para meu div.";
+minhaClasse.innerHTML = "<strong>Novo conteúdo para minha classe.</strong>";
+h1.textContent = "Novo conteúdo para meu H1.";
