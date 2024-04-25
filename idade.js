cf88a6c838ab47227fa63cfb7c69a0958e7fc428
Rogério Alves <rogerio.alves651@gmail.com>
2024-04-24 21:29:48 -0300

    Arquivos alterados

---
+++ idade.js
@@ -0,0 +1,10 @@
+let idade = 17;
+if (idade >= 18){
+    console.log("Você pode votar.");
+}
+else if (idade == 17){
+    console.log("Você pode votar no próximo ano.");
+}
+else {
+    console.log("Você ainda não pode votar.");
+}
