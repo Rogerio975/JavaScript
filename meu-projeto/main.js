f258aa180a482e3b1d3f721db4124957da09a4ec
Rogério Alves <rogerio.alves651@gmail.com>
2024-04-30 20:25:29 -0300

    Arquivo modificado.

--- meu-projeto/main.js
+++ meu-projeto/main.js
@@ -1,27 +1,27 @@
-//Selecionando elementos necessários
+// Selecionando elementos necessários.
 const form = document.querySelector('#task-form');
 const taskInput = document.querySelector('#task-input');
 const taskList = document.querySelector('#task-list');
 
-//Função para adicionar tarefa
+// Função para adicionar tarefa.
 function addTask(e) {
-    e.preventDefault(); //prevenindo o comportamento padrão do form
+    e.preventDefault(); // Prevenindo o comportamento padrão do form.
 
     if (taskInput.value.trim()) {
-        //se há algo escrito no input
-        //criação do elemento li
+        // Se há algo escrito no input.
+        // Criação do elemento li.
         const li = document.createElement('li');
         li.textContent = taskInput.value;
 
-        // adicionando a tarefa na lista
+        // Adicionando a tarefa na lista.
         taskList.appendChild(li);
 
-        // limpando o input
+        // Limpando o input.
         taskInput.value = "";
     } else {
         alert('Por favor, digite uma tarefa.');
     }
 }
 
-//Adicionando o evento de submit ao form.
+// Adicionando o evento de submit ao form.
 form.addEventListener('submit', addTask);
