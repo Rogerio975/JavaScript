c113e93668486e08d224bfa27fa68063a2eb34d9
Desktop <rogerio.alves651@gmail.com>
2024-08-04 14:59:59 -0300

    Pronto.

--- Alteração de cor/script.js
+++ Alteração de cor/script.js
@@ -3,4 +3,4 @@ function mudarCor() {
     const cores = ["red", "green", "blue", "purple", "orange", "rgb(139,69,19)", "#FF00FF"];
     const randomIndex = Math.floor(Math.random() * cores.length);
     div.style.color = cores[randomIndex];
-  }  
+  } 
