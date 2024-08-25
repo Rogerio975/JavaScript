8085ef8135acf5420a9b324ee7b79869c2b1a9d0
Desktop <rogerio.alves651@gmail.com>
2024-08-25 20:14:45 -0300

    Arquivo criado.

---
+++ slice.js
@@ -0,0 +1,5 @@
+const animais = ['formiga', 'boi', 'cavalo', 'águia', 'pato', 'elefante'];
+console.log(animais.slice(2));
+console.log(animais.slice(2,4));
+console.log(animais.slice(-1));
+console.log(animais.slice(2,-1));
