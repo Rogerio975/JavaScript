bb9314912552d4b547ea6815e2c42bd913e40e7f
Rogério <rogerio.alves651@gmail.com>
2025-01-01 18:13:13 -0300

    Projeto criado.

---
+++ React_Vite/my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>Bem-vindo ao nosso projeto React com Vite!</h1>
+      <p>Esta é a nossa primeira interação como o React. Em breve, adicionaremos mais funcionalidades.</p>
+
+    </div>
+  )
+}
+
+export default App
