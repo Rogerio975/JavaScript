fad98f85050f5eb1e0fcc70ccf2f41a13f86f14b
Rogério <rogerio.alves651@gmail.com>
2025-03-02 19:43:46 -0300

    Adicionar arquivos e configurações para módulos e dependências do projeto

--- my-api/index.js
+++ my-api/index.js
@@ -7,9 +7,14 @@ const app = express()
 app.use(helmet())
 
 app.get('/', (_request, response) => {
-  return response.json({
-    api: 'live'
-  })
+  response.setHeader('Content-Type', 'text/html');
+  return response.send(`
+    <html>
+      <body style="background-color: lightblue; color: white; font-family: Arial, sans-serif;">
+        <pre>${JSON.stringify({ api: 'live' }, null, 2)}</pre>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(port, () => {
