b78093dd0abcd7e2eb49f65d0a46aba588113392
Rogério Alves <rogerio.alves651@gmail.com>
2025-05-21 20:15:17 -0300

    Adicionar 'uva' ao array de frutas para incluir mais uma opção na lista.

--- shift.js
+++ shift.js
@@ -1,4 +1,4 @@
-let frutas = ['maçã', 'banana', 'laranja'];
+let frutas = ['uva', 'maçã', 'banana', 'laranja'];
 let primeiraFruta = frutas.shift();
 console.log(primeiraFruta);
 console.log(frutas);
