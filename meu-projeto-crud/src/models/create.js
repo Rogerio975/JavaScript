c0f9178bcc1057b1ff79f1aadc74f28defd23d38
Rogério <rogerio.alves651@gmail.com>
2026-05-15 20:56:32 -0300

    feat: implement createProduct function for product creation

---
+++ meu-projeto-crud/src/models/create.js
@@ -0,0 +1,11 @@
+const Product = require('./models/Product');
+
+// Dentro de uma função controladora (async/await)
+const createProduct = async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json({ success: true, data: product });
+    } catch (err) {
+        res.status(400).json({ success: false, error: err.message });
+    }
+};
