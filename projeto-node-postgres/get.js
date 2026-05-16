9bf7c5b7c4ad43d7cd0ec041c992aa6119547b73
Rogério <rogerio.alves651@gmail.com>
2026-05-16 01:09:10 -0300

    fix: remove unnecessary line from listarUsuarios function

--- projeto-node-postgres/get.js
+++ projeto-node-postgres/get.js
@@ -3,7 +3,6 @@ const pool = require('./db');
 async function listarUsuarios() {
 
     const resultado = await pool.query(
->>>>>>> 6d4868b82cf8328a533f7356852b2c220bb4efad
         'SELECT * FROM usuarios'
     );
 
